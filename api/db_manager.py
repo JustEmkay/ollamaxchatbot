9be33fb50a38c01768a229f162629085445211b9
JustEmkay <just.mk.things@gmail.com>
2024-10-23 21:24:15 +0530

    added database [sqlite]

--- api/db_manager.py
+++ api/db_manager.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect("../database/data.db",check_same_thread=False)
+conn.isolation_level = None
+cursor = conn.cursor()
+
 
 
 usersData : dict[dict] = {
@@ -8,5 +14,80 @@ usersData : dict[dict] = {
 }
 
 
+def create_Tables() -> dict:
+  """ create tables if not found.
+  Return: 
+    dictionary:
+  {
+    status (bool)
+    msg (str)
+  }
+  
+  """
+  
+  try: 
+    #memories
+    cursor.execute(  """CREATE TABLE IF NOT EXISTS memories(
+      memory_id INTEGER   PRIMARY KEY AUTOINCREMENT,
+      fragment TEXT   NOT NULL
+    )"""  )
+    
+  except sqlite3.Error as er:
+    print(f"errorcode:{er.sqlite_errorcode}\n \
+          errorname:{er.sqlite_errorname}") 
+    return {
+      'status' : False,
+      'msg' : 'error creating memories.'
+    }
+    
 
-tokensData : list = [  ]
+  try:
+    #assistants
+    cursor.execute(  """CREATE TABLE IF NOT EXISTS assistants(
+      assist_id INTEGER   PRIMARY KEY AUTOINCREMENT,
+      aname CHAR(20)    NOT NULL,
+      persona TEXT NOT NULL,
+      model TEXT    NOT NULL,
+      memory_id INTEGER   NOT_NULL,
+      FOREIGN KEY (memory_id)
+                      REFERENCES memories (memory_id)
+    )"""  )
+    
+  except sqlite3.Error as er:
+    print(f"errorcode:{er.sqlite_errorcode}\n \
+          errorname:{er.sqlite_errorname}") 
+    return {
+      'status' : False,
+      'msg' : 'error creating assistant.'
+    }  
+  
+  try:
+    #users
+    cursor.execute(  """CREATE TABLE IF NOT EXISTS users(
+      uid INTEGER   PRIMARY KEY AUTOINCREMENT,
+      assist_id INTEGER    NOT NULL,
+      username TEXT   NOT NULL,
+      qa TEXT   NOT NULL,
+      password BLOB   NOT NULL,
+      created_date INTEGER NOT NULL,
+      FOREIGN KEY (assist_id)
+                      REFERENCES assistants (assist_id)
+    )"""  ) 
+    
+  except sqlite3.Error as er:
+    print(f"errorcode:{er.sqlite_errorcode}\n \
+          errorname:{er.sqlite_errorname}") 
+    return {
+      'status' : False,
+      'msg' : 'error creating users.'
+    }
+
+  return {
+      'status' : True,
+      'msg' : 'Successful'
+    }
+
+
+
+if __name__ =='__main__':
+  create_Tables()
