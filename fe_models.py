57f069aede8754ece1c718e2b38b8399f59b1c38
JustEmkay <just.mk.things@gmail.com>
2024-10-24 15:46:16 +0530

    frontend validation model

---
+++ fe_models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class fe_RegisterInfo(BaseModel):
+    username : str
+    dob : int
+    aname : str
+    model : str
+    persona : str = None
+    qa : list
+    password : str
+
+class fe_logiInfo(BaseModel):
+    username : str
+    password : str
