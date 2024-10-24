513aaec4b72c9d9acdb9d939afe1df7f80def64a
JustEmkay <just.mk.things@gmail.com>
2024-10-24 22:12:20 +0530

    added user regiter data

--- api/db_manager.py
+++ api/db_manager.py
@@ -1,10 +1,12 @@
-import sqlite3
+import sqlite3,uuid
+from be_models import registerInfo
+from datetime import datetime
 
 conn = sqlite3.connect("../database/data.db",check_same_thread=False)
 conn.isolation_level = None
 cursor = conn.cursor()
 
-
+created_date = int(datetime.now().timestamp())
 
 usersData : dict[dict] = {
     "emkay" : {
@@ -25,12 +27,33 @@ def create_Tables() -> dict:
   }
   
   """
-  
+
+  try:
+    #assistants
+    cursor.execute(  """CREATE TABLE IF NOT EXISTS assistants(
+      assist_id TEXT   PRIMARY KEY,
+      aname CHAR(20)    NOT NULL,
+      persona TEXT,
+      model TEXT    NOT NULL
+    )"""  )
+    
+  except sqlite3.Error as er:
+    print(f"errorcode:{er.sqlite_errorcode}\n \
+          errorname:{er.sqlite_errorname}") 
+    return {
+      'status' : False,
+      'msg' : 'error creating assistant.'
+    }  
+ 
+ 
   try: 
     #memories
     cursor.execute(  """CREATE TABLE IF NOT EXISTS memories(
       memory_id INTEGER   PRIMARY KEY AUTOINCREMENT,
-      fragment TEXT   NOT NULL
+      assist_id TEXT NOT NULL,
+      fragment TEXT,
+      FOREIGN KEY (assist_id)
+                      REFERENCES assistant (assist_id)
     )"""  )
     
   except sqlite3.Error as er:
@@ -41,35 +64,16 @@ def create_Tables() -> dict:
       'msg' : 'error creating memories.'
     }
     
-
-  try:
-    #assistants
-    cursor.execute(  """CREATE TABLE IF NOT EXISTS assistants(
-      assist_id INTEGER   PRIMARY KEY AUTOINCREMENT,
-      aname CHAR(20)    NOT NULL,
-      persona TEXT NOT NULL,
-      model TEXT    NOT NULL,
-      memory_id INTEGER   NOT_NULL,
-      FOREIGN KEY (memory_id)
-                      REFERENCES memories (memory_id)
-    )"""  )
-    
-  except sqlite3.Error as er:
-    print(f"errorcode:{er.sqlite_errorcode}\n \
-          errorname:{er.sqlite_errorname}") 
-    return {
-      'status' : False,
-      'msg' : 'error creating assistant.'
-    }  
-  
+   
   try:
     #users
     cursor.execute(  """CREATE TABLE IF NOT EXISTS users(
-      uid INTEGER   PRIMARY KEY AUTOINCREMENT,
-      assist_id INTEGER    NOT NULL,
+      uid TEXT   PRIMARY KEY,
+      assist_id TEXT    NOT NULL,
       username TEXT   NOT NULL,
       dob INTEGER NOT NULL, 
-      qa TEXT   NOT NULL,
+      question INTEGER   NOT NULL,
+      answer TEXT   NOT NULL,
       password BLOB   NOT NULL,
       created_date INTEGER NOT NULL,
       FOREIGN KEY (assist_id)
@@ -89,7 +93,51 @@ def create_Tables() -> dict:
       'msg' : 'Successful'
     }
 
+def create_user(regData : registerInfo) -> dict:
+  
+  uid : str = str(uuid.uuid1())
+  assist_id : str = str(uuid.uuid3(uuid.NAMESPACE_DNS, regData.username))
+
+  
+  try :
+    cursor.execute( " INSERT INTO assistants(assist_id,aname,persona,model) \
+                  VALUES(?,?,?,?)",(assist_id,regData.aname,regData.persona,
+                                    regData.model,) )
+
+    cursor.execute( " INSERT INTO users(uid,assist_id,username,dob,question,answer,password,created_date) \
+                  VALUES(?,?,?,?,?,?,?,?)",(uid,assist_id,regData.username,regData.dob,regData.qa[0],
+                                    regData.qa[1],regData.password,created_date,) )
+    
+    cursor.execute( " INSERT INTO memories(assist_id) \
+                  VALUES(?)",(assist_id,) )
+
+    return {
+      'status' : True,
+      'msg' : 'Created account successfully.'
+    }
+
+  
+  except Exception as er:
+    print(f"error :{er}") 
+    return {
+      'status' : False,
+      'msg' : 'error creating users.'
+    }
 
 
 # if __name__ =='__main__':
-#   create_Tables()
+  # create_Tables()
+  # data = {"username":'emkay',
+  #              "dob":1729708200,
+  #              "aname":'juhi',
+  #              "model":'gemma2:2b',
+  #              "persona":'',
+  #              "qa":[0, 'doggy'],
+  #              "password":'Vasu@6969'}
+  
+  # reg = registerInfo(**data)
+  
+  # create_user(reg)
+
+# DB.uid : c638861c-9221-11ef-8781-d0c5d3da8dc4
+# DB.assist_id : 10fa9fde-db83-3fda-8fd2-3ea450795901
